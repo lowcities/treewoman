28f509c393d86bb13dbda081ab67308feac0500b
David Low <david.noyes.low@gmail.com>
2022-03-02 23:00:47 -0500

    Initial commit

---
+++ src/firebase/config.js
@@ -0,0 +1,22 @@
+import { initializeApp } from 'firebase/app';
+import  { getStorage } from 'firebase/storage';
+import { getFirestore } from 'firebase/firestore';
+
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyDMQdRcKfhx3FPU--xmhQ0hxBAiVQDfCPk",
+    authDomain: "treewoman-photo.firebaseapp.com",
+    projectId: "treewoman-photo",
+    storageBucket: "treewoman-photo.appspot.com",
+    messagingSenderId: "604294205709",
+    appId: "1:604294205709:web:c6ed7036b430ef3c33e044"
+  };
+
+  // Initialize Firebase
+  const firebaseApp = initializeApp(firebaseConfig);
+
+  const projectStorage = getStorage(firebaseApp);
+  const projectFirestore = getFirestore();
+
+  export { projectStorage, projectFirestore };
