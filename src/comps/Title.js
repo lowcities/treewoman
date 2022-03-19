0d885e9f3529ae19e354084ab712513360341a67
David Low <david.noyes.low@gmail.com>
2022-03-18 22:42:51 -0400

    added photo banner for title component

--- src/comps/Title.js
+++ src/comps/Title.js
@@ -4,8 +4,7 @@ const Title = () => {
   return (
     <div className="title">
       <h1>TreeWoman</h1>
-      <h2>In loving memory of Sarah Lucille Low</h2>
-      <p>Upload your pictures by clicking the button below</p>
+      <h2>Remembering and Celebrating Sarah Lucille Low</h2>
     </div>
   )
 }
